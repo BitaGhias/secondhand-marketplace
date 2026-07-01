7000462ce9d6073b6aeb2bb573fa800676015f9c
bita gh <yohoobitagh@gmail.com>
2026-07-01 18:01:46 +0330

    feat(backend): expose /api/auth/login endpoint in AuthController Accepts login requests from frontend and returns user data or error messages.

--- Backend/src/main/java/com/secondhand/backend/entity/User.java
+++ Backend/src/main/java/com/secondhand/backend/entity/User.java
@@ -1,20 +1,20 @@
 package com.secondhand.backend.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.*;//کد های ما رو به دیتابیس وصل میکنه
+import lombok.*;//جلوگیری از نوشتن کدهای تکراری
 
 @Entity
 @Table(name = "users")
-@Data                // ۲. تولید خودکار Getter، Setter، toString، equals و hashCode
-@NoArgsConstructor   // ۳. تولید سازنده بدون آرگومان
-@AllArgsConstructor  // ۴. تولید سازنده با تمام آرگومان‌ها
+@Data                //  تولید خودکار Getter، Setter، toString، equals و hashCode
+@NoArgsConstructor   //  تولید سازنده بدون آرگومان
+@AllArgsConstructor  //  تولید سازنده با تمام آرگومان‌ها
 public class User {
 
-    @Id
+    @Id //فیلد اصلی برای شناسایی
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;//شناسه کاربران
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true) //غیر تکراری و غیر خالی
     private String username;
 
     @Column(nullable = false)
