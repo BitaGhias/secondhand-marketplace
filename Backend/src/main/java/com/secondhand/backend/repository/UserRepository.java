930201ac16a288e6ce647114f0552281863ebe88
bita gh <yohoobitagh@gmail.com>
2026-07-01 17:51:24 +0330

    feat(backend): add findByUsername method to UserRepository

--- Backend/src/main/java/com/secondhand/backend/repository/UserRepository.java
+++ Backend/src/main/java/com/secondhand/backend/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.secondhand.backend.repository;
-
+//کارمند دیتابیس
 import com.secondhand.backend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;//ابزار اصلی DB
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long>
 {
     boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username); //برای خطا ندادن برنامه در صورت پیدا نکردن
 }
