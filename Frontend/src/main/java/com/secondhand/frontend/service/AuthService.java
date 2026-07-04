f83ea8306f074393b8739cb74035f0387fb6cf80
Ata <ata.tz@outlook.com>
2026-07-04 18:16:41 +0330

    feat: added AuthService

---
+++ Frontend/src/main/java/com/secondhand/frontend/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.secondhand.frontend.service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class AuthService
+{
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+    private final String BASE_URL = "http://localhost:8080/api";
+    public boolean login(String username, String password)
+    {
+        try
+        {
+            String jsonInput = String.format("{\"username\":\"%s\",\"password\":\"%s\"}", username, password);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(BASE_URL + "/login"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonInput))
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.statusCode() == 200;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
