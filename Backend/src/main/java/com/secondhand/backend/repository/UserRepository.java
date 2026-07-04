773dfec16fc1952132f855f05f0a21bdcdc0bb10
Ata <ata.tz@outlook.com>
2026-07-04 18:49:17 +0330

    feat(backend): add findByUsername to UserRepository

--- Backend/src/main/java/com/secondhand/backend/repository/UserRepository.java
+++ Backend/src/main/java/com/secondhand/backend/repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.secondhand.backend.repository;
 
 import com.secondhand.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long>
 {
+    // برای ثبت‌نام (چک کردن تکراری نبودن یوزر)
     boolean existsByUsername(String username);
+
+    // برای لاگین (بیرون کشیدن پرونده کاربر)
+    Optional<User> findByUsername(String username);
 }
