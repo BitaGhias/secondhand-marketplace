94c5f494e85bf28aa72dcb8c6801cdb40f09cde0
bita gh <yohoobitagh@gmail.com>
2026-07-07 18:57:36 +0330

    chore: remove database files from git and add to gitignore

--- Backend/src/main/java/com/secondhand/backend/config/PasswordEncoderConfig.java
+++ Backend/src/main/java/com/secondhand/backend/config/PasswordEncoderConfig.java
@@ -3,12 +3,13 @@ package com.secondhand.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder; //برای رعایت قوانین هش پسورد
 
-@Configuration
+//برای هش کردن پسورد و تبدیل ان به متنی غیر قابل فهم
+@Configuration //کلاس تنظیمات
 public class PasswordEncoderConfig {
 
-    @Bean
+    @Bean //ساخت ابزاری برای استفاده از بقیه
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
--- Backend/src/main/java/com/secondhand/backend/util/JwtUtil.java
+++ Backend/src/main/java/com/secondhand/backend/util/JwtUtil.java
@@ -1,21 +1,23 @@
-package com.secondhand.backend.util;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+package com.secondhand.backend.util;//ابزار
+//قلب سیستم احراز هویت
+import io.jsonwebtoken.Claims;//برای همه اطلاعاتی که توی توکن ذخیره شده
+import io.jsonwebtoken.Jwts;//کارخونه ی ساخت توکن
+import io.jsonwebtoken.security.Keys;//تبدیل درست کلید مخفی به فرمت درست
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
+import javax.crypto.SecretKey;//کلیدمخفی برای امضای توکن
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
     //  کلید مخفی برای امضای توکن
+    //برای جلوگیری از جعل توکن
     private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
     //  مدت اعتبار توکن (۲۴ ساعت)
-    private static final long EXPIRATION_TIME = 24 * 60 * 60 * 1000; // 24 ساعت
+    //یعنی بعد 24 ساعت دوباره باید لاگین کنه
+    private static final long EXPIRATION_TIME = 24 * 60 * 60 * 1000;
 
     //  گرفتن کلید مخفی
     private SecretKey getSigningKey() {
@@ -25,7 +27,7 @@ public class JwtUtil {
     //  ساختن توکن جدید
     public String generateToken(String username, Long userId, String role) {
         return Jwts.builder()
-                .subject(username)                    // username
+                .subject(username)                    // username و موضوع توکن
                 .claim("userId", userId)              // userId
                 .claim("role", role)                  // role
                 .issuedAt(new Date())                 // زمان ساخت
@@ -41,8 +43,9 @@ public class JwtUtil {
 
     //  گرفتن userId از توکن
     public Long extractUserId(String token) {
-        return extractAllClaims(token).get("userId", Long.class);
-    }
+        return extractAllClaims(token).get("userId", Long.class); //به صورت لانگ برگردون
+
+} //به صورت لانگ برگردون
 
     //  گرفتن role از توکن
     public String extractRole(String token) {
@@ -63,9 +66,9 @@ public class JwtUtil {
     // استخراج همه اطلاعات از توکن
     private Claims extractAllClaims(String token) {
         return Jwts.parser()
-                .verifyWith(getSigningKey())
-                .build()
+                .verifyWith(getSigningKey()) //چک کردن امضای توکن
+                .build() //اماده کردن رمزگشا
                 .parseSignedClaims(token)
-                .getPayload();
+                .getPayload(); //برگردوندن بخش دوم توکن
     }
 }
