ea6e5093e0ea109ae71539b517b87afae2926402
Ata <ata.tz@outlook.com>
2026-07-12 14:19:10 +0330

    refactor: changed MainApplication.java

--- Frontend/src/main/java/com/secondhand/frontend/MainApplication.java
+++ Frontend/src/main/java/com/secondhand/frontend/MainApplication.java
@@ -2,21 +2,46 @@ package com.secondhand.frontend;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainApplication extends Application {
 
+    private static Stage primaryStage;
+    private static final int DEFAULT_WIDTH = 500;
+    private static final int DEFAULT_HEIGHT = 650;
+
     @Override
     public void start(Stage stage) throws Exception {
+        primaryStage = stage;//برای تغییر صحنه از جاهای مختلف برنامه
+
+        // لود کردن صفحه ورود
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/secondhand/frontend/login.fxml"));
-        Scene scene = new Scene(loader.load(), 500, 420);
-        stage.setTitle("Second-hand Project");
+        Parent root = loader.load();//شامل همه چیزهایی که تو صفحه نمایش داده میشه
+
+        Scene scene = new Scene(root, DEFAULT_WIDTH, DEFAULT_HEIGHT);//اولی منظور محتوای توی صفحه
+        scene.getStylesheets().add(getClass().getResource("/com/secondhand/frontend/css/styles.css").toExternalForm());
+
+        stage.setTitle("فروشگاه دست دوم - ورود");
         stage.setScene(scene);
+        stage.setResizable(false);//صفحات ورود و ثبت نام غیرقابل تغییر
+        stage.initStyle(StageStyle.UNDECORATED);  // حذف title bar
         stage.show();
     }
 
+    public static void changeScene(String fxmlPath, String title) throws Exception {
+        FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource(fxmlPath));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        scene.getStylesheets().add(MainApplication.class.getResource("/com/secondhand/frontend/css/styles.css").toExternalForm());
+        primaryStage.setTitle(title);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
